82ff528afb9d83a51f925959ff22f658ac0ec1f1
Ted <Ted.Chiue@ibm.com>
2023-02-15 00:06:58 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  {{this.aa}}
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios"
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data(){
+    return {
+      aa:null
+    }
+  },
+  mounted(){
+    axios.get("/api/todo2_16").then((res)=> {
+      console.log(res.data)
+      this.aa = res.data
+    })
   }
 }
 </script>
